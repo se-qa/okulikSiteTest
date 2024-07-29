7e80ad673c63565eb576bcec4b852c3c566ea386
Dozer <shitov.ea@gmail.com>
2024-07-29 23:16:56 +0400

    Opening url moved to page fixtures instead of opening url in each test Removed unnecessary driver fixture call in tests The driver is already passed to the fixture of each page

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ from pages.recovery_page import RecoveryPage
 from pages.register_page import RegisterPage
 from pages.video_page import VideoPage
 from pages.home_page import HomePage
-from utils.client import load_config, get_chrome_options
+from utils.client import load_config, get_chrome_options, URL, VIDEO, PERSON, LOGIN, RESET, REGISTER
 
 
 @pytest.fixture
@@ -23,29 +23,35 @@ def driver() -> webdriver:
 
 @pytest.fixture
 def home_page(driver: webdriver) -> HomePage:
+    driver.get(URL)
     return HomePage(driver)
 
 
 @pytest.fixture
 def video_page(driver: webdriver) -> VideoPage:
+    driver.get(VIDEO)
     return VideoPage(driver)
 
 
 @pytest.fixture
 def person_page(driver: webdriver) -> PersonPage:
+    driver.get(PERSON)
     return PersonPage(driver)
 
 
 @pytest.fixture
 def login_page(driver: webdriver) -> LoginPage:
+    driver.get(LOGIN)
     return LoginPage(driver)
 
 
 @pytest.fixture
 def recovery_page(driver: webdriver) -> RecoveryPage:
+    driver.get(RESET)
     return RecoveryPage(driver)
 
 
 @pytest.fixture
 def register_page(driver: webdriver) -> RegisterPage:
+    driver.get(REGISTER)
     return RegisterPage(driver)
--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -11,37 +11,32 @@ from tests.data.tests_data.parametrize_home_page import *
 from utils.client import URL, PRACTICAL_TASK
 
 
-def test_home_page(driver, home_page):
-    home_page.open(URL)
+def test_home_page(home_page):
     assert home_page.is_current_url(URL)
     assert home_page.is_element_visible(link_logo)
 
 
-def test_logo_click(driver, home_page):
-    home_page.open(URL)
+def test_logo_click(home_page):
     home_page.click_element(link_logo)
     assert home_page.is_current_url(URL)
     assert home_page.is_element_in_viewport(link_logo)
 
 
-def test_button_sign_up_click(driver, home_page):
-    home_page.open(URL)
+def test_button_sign_up_click(home_page):
     home_page.click_element(button_sign_up_top)
     home_page.wait_for_scroll_to_element(title_booking)
     assert home_page.is_element_in_viewport(title_booking)
 
 
 @pytest.mark.parametrize("anchor, target", anchors_top_list, ids=anchors_top_list_ids)
-def test_top_anchor_click(driver, home_page, anchor, target):
-    home_page.open(URL)
+def test_top_anchor_click(home_page, anchor, target):
     home_page.click_element(anchor)
     home_page.wait_for_scroll_to_element(target)
     assert home_page.is_element_in_viewport(target)
 
 
 @pytest.mark.parametrize("link, target, url", buy_links_list, ids=buy_links_list_ids)
-def test_buy_course_links_click(driver, home_page, link, target, url):
-    home_page.open(URL)
+def test_buy_course_links_click(home_page, link, target, url):
     home_page.click_element(link)
     home_page.wait_for_element_visible_by_locator(target)
     assert home_page.is_current_url(url)
@@ -49,8 +44,7 @@ def test_buy_course_links_click(driver, home_page, link, target, url):
 
 
 @pytest.mark.parametrize("link, target, url", other_links_list, ids=other_links_list_ids)
-def test_other_links_click(driver, home_page, link, target, url):
-    home_page.open(URL)
+def test_other_links_click(home_page, link, target, url):
     home_page.scroll_to_element(link)
     home_page.click_element(link)
     home_page.wait_for_element_visible_by_locator(target)
@@ -58,23 +52,20 @@ def test_other_links_click(driver, home_page, link, target, url):
     assert home_page.is_element_visible(target)
 
 
-def test_anchor_learning_process_click(driver, home_page):
-    home_page.open(URL)
+def test_anchor_learning_process_click(home_page):
     home_page.scroll_to_element(anchor_learning_process)
     home_page.click_element(anchor_learning_process)
     home_page.wait_for_scroll_to_element(title_learning_process)
     assert home_page.is_element_in_viewport(title_learning_process)
 
 
-def test_collapse_cards_click(driver, home_page):
-    home_page.open(URL)
+def test_collapse_cards_click(home_page):
     home_page.scroll_to_element_top_of_screen(collapse_cards)
     home_page.wait_for_scroll_to_element(collapse_cards)
     home_page.click_all_collapse_elements_with_class(collapse_cards, "collapsed")
 
 
-def test_button_sign_up_right_group_click(driver, home_page):
-    home_page.open(URL)
+def test_button_sign_up_right_group_click(home_page):
     home_page.scroll_to_element(button_sign_up_right_group)
     home_page.wait_for_scroll_to_element(button_sign_up_right_group)
     home_page.click_element(button_sign_up_right_group)
@@ -82,8 +73,7 @@ def test_button_sign_up_right_group_click(driver, home_page):
     assert home_page.is_element_in_viewport(title_booking)
 
 
-def test_switchers_click(driver, home_page):
-    home_page.open(URL)
+def test_switchers_click(home_page):
     home_page.scroll_to_element(switcher_stages)
     home_page.wait_for_scroll_to_element(switcher_stages)
     assert home_page.is_element_visible(title_staged_payment)
@@ -94,8 +84,7 @@ def test_switchers_click(driver, home_page):
     assert home_page.is_element_visible(title_staged_payment)
 
 
-def test_button_pay_first_step_click(driver, home_page):
-    home_page.open(URL)
+def test_button_pay_first_step_click(home_page):
     home_page.scroll_to_element(button_pay_first_step)
     home_page.wait_for_scroll_to_element(button_pay_first_step)
     home_page.click_element(button_pay_first_step)
@@ -104,8 +93,7 @@ def test_button_pay_first_step_click(driver, home_page):
     assert home_page.is_element_in_viewport(title_part_price)
 
 
-def test_button_pay_entirely_click(driver, home_page):
-    home_page.open(URL)
+def test_button_pay_entirely_click(home_page):
     home_page.scroll_to_element(switcher_entire_amount)
     home_page.wait_for_scroll_to_element(switcher_entire_amount)
     home_page.click_element(switcher_entire_amount)
@@ -117,8 +105,7 @@ def test_button_pay_entirely_click(driver, home_page):
     assert home_page.is_element_in_viewport(title_full_price)
 
 
-def test_button_get_practical_task_click(driver, home_page):
-    home_page.open(URL)
+def test_button_get_practical_task_click(home_page):
     home_page.scroll_to_element(button_get_a_practical_task)
     home_page.wait_for_scroll_to_element(button_get_a_practical_task)
     home_page.click_element(button_get_a_practical_task)
@@ -127,8 +114,7 @@ def test_button_get_practical_task_click(driver, home_page):
     assert home_page.is_element_in_viewport(title_practical_task)
 
 
-def test_carousel_previous_button_click(driver, home_page):
-    home_page.open(URL)
+def test_carousel_previous_button_click(home_page):
     home_page.scroll_to_element_top_of_screen(div_carousel)
     home_page.wait_for_scroll_to_element(div_carousel)
     home_page.click_element(button_carousel_previous)
@@ -136,8 +122,7 @@ def test_carousel_previous_button_click(driver, home_page):
     assert home_page.is_element_in_viewport(img_carousel_item_7)
 
 
-def test_carousel_next_button_click(driver, home_page):
-    home_page.open(URL)
+def test_carousel_next_button_click(home_page):
     home_page.scroll_to_element_top_of_screen(div_carousel)
     home_page.wait_for_scroll_to_element(div_carousel)
     home_page.click_element(button_carousel_next)
@@ -145,8 +130,7 @@ def test_carousel_next_button_click(driver, home_page):
     assert home_page.is_element_in_viewport(img_carousel_item_2)
 
 
-def test_click_all_carousel_pages(driver, home_page):
-    home_page.open(URL)
+def test_click_all_carousel_pages(home_page):
     home_page.scroll_to_element_top_of_screen(div_carousel)
     home_page.wait_for_scroll_to_element(div_carousel)
     home_page.click_all_carousel_elements(buttons_carousel)
